eec332bbe607961ee5fd9627a2f93e797cdec03b
Issac-Kim <kimi6@rpi.edu>
2019-03-12 22:32:49 -0400

    add new photo

--- 6/word_ladder.py
+++ 6/word_ladder.py
@@ -55,13 +55,13 @@ def generate_graph(words):
 
 def words_graph():
     """Return the words example graph from the Stanford GraphBase"""
-    fh = gzip.open('words_dat.txt.gz', 'r')
+    fh = gzip.open('words4_dat.txt.gz', 'r')
     words = set()
     for line in fh.readlines():
         line = line.decode()
         if line.startswith('*'):
             continue
-        w = str(line[0:5])
+        w = str(line[0:4])
         words.add(w)
     return generate_graph(words)
 
@@ -73,10 +73,8 @@ if __name__ == '__main__':
           % (nx.number_of_nodes(G), nx.number_of_edges(G)))
     print("%d connected components" % nx.number_connected_components(G))
 
-    for (source, target) in [('chaos', 'order'),
-                             ('nodes', 'graph'),
-                             ('moron', 'smart'),
-                             ('pound', 'marks')]:
+    for (source, target) in [('love', 'hate'),
+                             ('cold', 'warm')]:
         print("Shortest path between %s and %s is" % (source, target))
         try:
             sp = nx.shortest_path(G, source, target)
